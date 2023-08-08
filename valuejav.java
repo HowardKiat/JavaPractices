19486647c3a3f7edba1820ccae833b5cffbbb8aa
HowardKiat <118371226+HowardNoobCoder@users.noreply.github.com>
2023-08-08 09:14:21 +0800

    Add files via upload

---
+++ valuejav.java
@@ -0,0 +1,8 @@
+public class valuejav {
+    public static void main(String[] args){
+        float price = 45.35f;
+        int units = 10;
+        System.out.println(price*units);
+        
+    }
+}
